b08f3ec40e3d56890832e53e2036f726c4f6e3c8
Joshua Rosenberg <jrosen@msu.edu>
2017-06-28 19:40:24 -0400

    update docs

--- R/functions.R
+++ R/functions.R
@@ -6,8 +6,8 @@
 #' @param n_covariate the number of covariates in the regression model
 #' @return prints the bias and the number of cases that would have to be replaced with cases for which there is no effect to invalidate the inference
 #' @examples
-#' pkonfound(2, .4, 100, 3)
-#' pkonfound(.4, 2, 100, 3)
+#' pkonfound(unstd_beta = 2, standard_error = .4, n_obs = 100, n_covariates = 3)
+#' pkonfound(unstd_beta = .4, standard_error = 2, n_obs = 100, n_covariates = 3)
 #' @export
 
 pkonfound <- function(unstd_beta,
@@ -16,28 +16,26 @@ pkonfound <- function(unstd_beta,
                       n_covariates = 3, 
                       alpha = .05, 
                       tails = 2) {
-
-    critical_t <- qt(1 - (alpha / tails), n_obs - n_covariates)
+    
+    if (unstd_beta < 0) {
+        critical_t <- qt(1 - (alpha / tails), n_obs - n_covariates) * -1
+    } else {
+        critical_t <- qt(1 - (alpha / tails), n_obs - n_covariates)
+    }
     
     beta_threshhold <- critical_t * standard_error
     
-    if (unstd_beta > beta_threshhold) {
+    if (abs(unstd_beta) > abs(beta_threshhold)) {
         bias <- 100 * (1 - (beta_threshhold / unstd_beta))
         recase <- round(n_obs * (bias / 100))
         cat(paste0("To invalidate the inference, ", round(bias, 2), " % of the estimate would have to be due to bias.\n"))
         cat(paste0("To invalidate the inference, ", round(recase, 3), " observations would have to be replaced with cases for which there is no effect."))
-    } else if (unstd_beta < beta_threshhold) {
-        bias <- 100 * (1 - (unstd_beta / beta_threshhold))
-        recase <- round(n_obs * (bias / 100))
-        cat(paste0("To sustain the inference, ", round(bias, 2), " % of the estimate would have to be due to bias.\n"))
+    } else if (abs(unstd_beta) < abs(beta_threshhold)) {
+        sustain <- 100 * (1 - (unstd_beta / beta_threshhold))
+        recase <- round(n_obs * (sustain / 100))
+        cat(paste0("To sustain the inference, ", round(sustain, 2), " % of the estimate would have to be due to bias.\n"))
         cat(paste0("To sustain the inference, ", round(recase, 3), " of the cases with 0 effect would have to be replaced with cases at the threshold of inference."))
     } else if (unstd_beta == beta_threshhold) {
         warning("The coefficient is exactly equal to the threshold.")
     }
-    
 }
-
-### Questions for Ran
-
-# Am I thinking about invalidate / sustain the right way?
-# What does rep_0 do?
--- README.md
+++ README.md
@@ -9,12 +9,12 @@ Because this package is not on [CRAN](https://cran.r-project.org/), it must be d
     
 Currently, one function is in-development, `pkonfound`, for published studies, this command calculates (1) how much bias there must be in an estimate to invalidate/sustain an inference; (2) the impact of an omitted variable necessary to invalidate/sustain an inference for a regression coefficient.
 
-    pkonfound(2, .4, 100, 3)
+    konfound(unstd_beta = 2, standard_error = .4, n_obs = 100, n_covariates = 3)
 
     # To invalidate the inference, 60.31 % of the estimate would have to be due to bias.
     # To invalidate the inference, 60 observations would have to be replaced with cases for which there is no effect.
 
-    pkonfound(.4, 2, 100, 3)
+    pkonfound(unstd_beta = .4, standard_error = 2, n_obs = 100, n_covariates = 3)
     
     # To sustain the inference, 89.92 % of the estimate would have to be due to bias.
     # To sustain the inference, 90 of the cases with 0 effect would have to be replaced with cases at the threshold of inference.
