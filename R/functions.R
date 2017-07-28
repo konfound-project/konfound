88c1e6ddd1aa56c68af9ec1d76952c41acbcbf54
Joshua Rosenberg <jrosen@msu.edu>
2017-07-28 10:06:15 -0400

    move main function out of pkonfound()

--- R/functions.R
+++ R/functions.R
@@ -1,22 +1,11 @@
-#' Perform sensitivity analysis for published studies
-#' @description For published studies, this command calculates (1) how much bias there must be in an estimate to invalidate/sustain an inference; (2) the impact of an omitted variable necessary to invalidate/sustain an inference for a regression coefficient.
-#' @param unstd_beta an unstandardized regression coefficient
-#' @param standard_error the standard error of the estimate of the unstandardized regression coefficient
-#' @param n_obs the number of observations in the sample
-#' @param n_covariate the number of covariates in the regression model
-#' @return prints the bias and the number of cases that would have to be replaced with cases for which there is no effect to invalidate the inference
-#' @examples
-#' pkonfound(unstd_beta = 2, standard_error = .4, n_obs = 100, n_covariates = 3)
-#' pkonfound(unstd_beta = .4, standard_error = 2, n_obs = 100, n_covariates = 3)
-#' @export
+# Main function to test sensitivity to be wrapped with pkonfound() and konfound()
 
-pkonfound <- function(unstd_beta,
-                      standard_error,
-                      n_obs, 
-                      n_covariates = 3, 
-                      alpha = .05, 
-                      tails = 2) {
-    
+test_sensitivity <- function(unstd_beta,
+                             standard_error,
+                             n_obs, 
+                             n_covariates,
+                             alpha, 
+                             tails) {
     if (unstd_beta < 0) {
         critical_t <- qt(1 - (alpha / tails), n_obs - n_covariates) * -1
     } else {
@@ -28,15 +17,42 @@ pkonfound <- function(unstd_beta,
     if (abs(unstd_beta) > abs(beta_threshhold)) {
         bias <- 100 * (1 - (beta_threshhold / unstd_beta))
         recase <- round(n_obs * (bias / 100))
-        cat(paste0("To invalidate the inference, ", round(bias, 2), " % of the estimate would have to be due to bias.\n"))
+        cat(paste0("To invalidate the inference, ", round(bias, 2), "% of the estimate would have to be due to bias.\n"))
         cat(paste0("To invalidate the inference, ", round(recase, 3), " observations would have to be replaced with cases for which there is no effect."))
     } else if (abs(unstd_beta) < abs(beta_threshhold)) {
         sustain <- 100 * (1 - (unstd_beta / beta_threshhold))
         recase <- round(n_obs * (sustain / 100))
-        cat(paste0("To sustain the inference, ", round(sustain, 2), " % of the estimate would have to be due to bias.\n"))
+        cat(paste0("To sustain the inference, ", round(sustain, 2), "% of the estimate would have to be due to bias.\n"))
         cat(paste0("To sustain the inference, ", round(recase, 3), " of the cases with 0 effect would have to be replaced with cases at the threshold of inference."))
     } else if (unstd_beta == beta_threshhold) {
         warning("The coefficient is exactly equal to the threshold.")
     }
+}
+
+#' Perform sensitivity analysis for published studies
+#' @description For published studies, this command calculates (1) how much bias there must be in an estimate to invalidate/sustain an inference; (2) the impact of an omitted variable necessary to invalidate/sustain an inference for a regression coefficient.
+#' @param unstd_beta an unstandardized regression coefficient
+#' @param standard_error the standard error of the estimate of the unstandardized regression coefficient
+#' @param n_obs the number of observations in the sample
+#' @param n_covariate the number of covariates in the regression model
+#' @return prints the bias and the number of cases that would have to be replaced with cases for which there is no effect to invalidate the inference
+#' @examples
+#' pkonfound(2, .4, 100, 3)
+#' pkonfound(.4, 2, 100, 3)
+#' @export
+
+pkonfound <- function(unstd_beta,
+                      standard_error,
+                      n_obs, 
+                      n_covariates = 1, 
+                      alpha = .05, 
+                      tails = 2) {
+    
+    test_sensitivity(unstd_beta = unstd_beta,
+                     standard_error = standard_error,
+                     n_obs = n_obs, 
+                     n_covariates = n_covariates,
+                     alpha = alpha, 
+                     tails = tails)
     
 }
