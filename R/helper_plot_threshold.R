5883b8d208b2fb6bafc4ba07fcac4475752c2f1e
Joshua Rosenberg <jrosen@msu.edu>
2017-10-28 00:44:35 -0400

    update docs and vignettes

--- R/helper_plot_threshold.R
+++ R/helper_plot_threshold.R
@@ -1,8 +1,7 @@
 # Function to output the plot
 
 plot_threshold <- function(beta_threshold, unstd_beta) {
-    
-    if (unstd_beta > beta_threshold) { # beta is above threshold
+    if (abs(unstd_beta) > abs(beta_threshold)) { # beta is above threshold
         dd <- dplyr::data_frame(unstd_beta = unstd_beta, beta_threshold = beta_threshold)
         dd <- dplyr::mutate(dd, `Above Threshold` = unstd_beta - beta_threshold)
         dd <- dplyr::rename(dd, `Below Threshold` = beta_threshold)
@@ -13,11 +12,14 @@ plot_threshold <- function(beta_threshold, unstd_beta) {
         
         y_thresh <- dplyr::filter(dd, key == "Below Threshold")
         y_thresh <- dplyr::pull(dplyr::select(y_thresh, val))
-        y_thresh_text <- y_thresh + sqrt(.005 * y_thresh)
+        y_thresh_text <- y_thresh + sqrt(.005 * abs(y_thresh))
+        
+        effect_text <- unstd_beta + (.025 * unstd_beta) # y-value of text
         
         cols <- c("#A6CEE3", "#1F78B4") # dark blue and light blue
         
-    } else if (unstd_beta < beta_threshold) { # beta is below threshold
+    } else if (abs(unstd_beta) < abs(beta_threshold)) { # beta is below threshold
+        print("hi!")
         dd <- dplyr::data_frame(unstd_beta = unstd_beta, beta_threshold = beta_threshold)
         dd <- dplyr::mutate(dd, `Above Threshold` = abs(unstd_beta - beta_threshold))
         dd <- dplyr::mutate(dd, `Below Threshold` = unstd_beta)
@@ -27,15 +29,22 @@ plot_threshold <- function(beta_threshold, unstd_beta) {
         dd <- tidyr::gather(dd, key, val)
         dd <- dplyr::mutate(dd, inference = "group")
         
-        y_thresh <- sum(dd$val)
-        y_thresh_text <- y_thresh + sqrt(.005 * y_thresh)
+        print(dd)
+        y_thresh <- sum(abs(dd$val))
+        if (unstd_beta < 0) {
+            y_thresh <- y_thresh * -1
+        }
         
-        cols <- c("#B2DF8A", "#1F78B4") # dark blue and green
-    
+        y_thresh_text <- y_thresh + sqrt(.005 * abs(y_thresh))
+        effect_text <- unstd_beta + sqrt(.025 * abs(unstd_beta)) # y-value of text
+        
+        dd <- dplyr::filter(dd, key != "Above Threshold")
+        
+        cols <- c("#B2DF8A", "#1F78B4") # dark blue and green (green not used right now)
+        
     }
-    
-    effect_text <- unstd_beta + (.025 * unstd_beta) # y-value of text
 
+    
     p <- ggplot2::ggplot(dd, ggplot2::aes(x = inference, y = val, fill = key)) +
         ggplot2::geom_col(position = "stack") +
         
@@ -46,13 +55,10 @@ plot_threshold <- function(beta_threshold, unstd_beta) {
         ggplot2::annotate("text", x = 1, y = y_thresh_text, label = "Threshold") +
         
         ggplot2::scale_fill_manual("", values = cols) +
-        ggplot2::labs(caption = "The solid black line represents the effect") +
         ggplot2::theme_bw() +
         ggplot2::theme(axis.text.x = ggplot2::element_blank(), axis.ticks = ggplot2::element_blank()) +
         ggplot2::xlab(NULL) +
-        ggplot2::ylab("Unstandardized Beta Coefficient") +
-        
-        ggplot2::labs(caption = "Correlations are conditional on observed covariates")
+        ggplot2::ylab("Unstandardized Beta Coefficient")
     
     return(p)
 } 
