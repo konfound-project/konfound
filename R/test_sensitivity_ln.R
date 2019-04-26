d2123e3a862ca985d0ce72dcd00412f6841f41eb
Qinyun Lin <linqinyu@msu.edu>
2019-04-26 16:32:21 -0400

    add in the raw konfoundl function
    I added in the raw konfoundl function in the end of the document.

--- R/test_sensitivity_ln.R
+++ R/test_sensitivity_ln.R
@@ -21,7 +21,7 @@
 
 # Main function to test sensitivity to be wrapped with pkonfound(), konfound(), and mkonfound()
 
-test_sensitivity <- function(est_eff,
+test_sensitivity_nl <- function(est_eff,
                              std_err,
                              n_obs,
                              n_covariates,
@@ -124,3 +124,133 @@ test_sensitivity <- function(est_eff,
     stop("to_return must be set to 'raw_output', 'print', 'table', 'thresh_plot', or 'corr_plot' or some combination thereof")
   }
 }
+
+konfoundl <- function(est_eff, std_err, n_obs, n_trm, switch_trm = TRUE, thr_t = 1.96) {
+# Get input for estimate, standard error, sample size & one marginal
+# est_eff <- -0.2
+# std_err <- 0.10383
+# n_obs <- 20888
+# n_trm <- 17888
+# switch_trm <- F
+# thr_t <- -15
+
+# stop message
+if (n_obs <= 0 || n_trm <= 0) 
+  {stop("Please enter positive integers for sample size and number of treatment group cases!")}
+if (n_obs <= n_trm) 
+  {stop("The total sample size should be larger than the number of treatment group cases.")}
+
+odds_ratio <- exp(est_eff)
+# n_trm is the number of observations in the treatment group (c+d)
+# n_cnt is the number of observations in the control group (a+b)
+n_cnt <- n_obs - n_trm
+# t_ob is the t value calculated based on observed estimate and standard error
+t_ob <- est_eff/std_err
+# invalidate_ob is true - the observed result is significant - we are invalidating the observed result
+invalidate_ob <- isinvalidate(thr_t,t_ob)
+# dcroddsratio_ob is true - our goal is to decrease the odds ratio
+dcroddsratio_ob <- isdcroddsratio(thr_t, t_ob)
+
+# a1, b1, c1, d1 are one solution for the 4 cells in the contingency table
+a1 <- get_a1_kfnl(odds_ratio, std_err, n_obs, n_trm)
+b1 <- n_cnt - a1
+c1 <- get_c1_kfnl(odds_ratio, std_err, n_obs, n_trm)
+d1 <- n_trm - c1
+
+# a2, b2, c2, d2 are the second solution for the 4 cells in the contingency table
+a2 <- get_a2_kfnl(odds_ratio, std_err, n_obs, n_trm)
+b2 <- n_cnt - a2
+c2 <- get_c2_kfnl(odds_ratio, std_err, n_obs, n_trm)
+d2 <- n_trm - c2
+
+# Differences between these two sets of solutions: 
+### a1 c1 are small while a2 c2 are large
+### b1 d1 are large while b2 d2 are small
+### the goal is to get fewest swithes to invalidate the inference 
+### remove the solution if one cell has fewerer than 5 cases or negative cells 
+check1 <- check2 <- TRUE
+if (!(n_cnt>=a1 && a1>5 && n_cnt>=b1 && b1>5 && n_trm>=c1 && c1>5 && n_trm>=d1 && d1>5))
+  {check1 <- FALSE}
+if (!(n_cnt>=a2 && a2>5 && n_cnt>=b2 && b2>5 && n_trm>=c2 && c2>5 && n_trm>=d2 && d2>5))
+  {check2<- FALSE}
+if (check1) {
+  table_bstart1 <- get_abcd_kfnl(a1, b1, c1, d1)
+  solution1 <- getswitch(table_bstart1, thr_t, switch_trm, n_obs)
+  }
+if (check2) {
+  table_bstart2 <- get_abcd_kfnl(a2, b2, c2, d2)
+  solution2 <- getswitch(table_bstart2, thr_t, switch_trm, n_obs)
+  }
+if (!check1 && !check2)
+  {stop("Cannot generate a reasonable contingency table!")}
+
+# get the number of switches for solutions that satisfy the requirements
+ if (check1 && check2) {
+   final_switch1 <- solution1$final_switch
+   final_switch2 <- solution2$final_switch
+   if (final_switch1 < final_switch2) {
+   final_solution <- getswitch(table_bstart1, thr_t, switch_trm, n_obs)
+ } else {
+   final_solution <- getswitch(table_bstart2, thr_t, switch_trm, n_obs)
+ }
+ } 
+
+if (check1 && !check2) {
+    final_solution <- getswitch(table_bstart1, thr_t, switch_trm, n_obs)
+  }
+
+if (!check1 && check2) {
+  final_solution <- getswitch(table_bstart2, thr_t, switch_trm, n_obs)
+}
+
+
+if (switch_trm && dcroddsratio_ob) 
+{transferway <- "treatment success to treatment failure,"} 
+if (switch_trm && !dcroddsratio_ob) 
+{transferway <- "treatment failure to treatment success,"} 
+if (!switch_trm && dcroddsratio_ob) 
+{transferway <- "control failure to control success,"} 
+if (!switch_trm && !dcroddsratio_ob) 
+{transferway <- "control success to control failure,"}
+
+if (final_solution$needtworows) {
+  if (switch_trm && dcroddsratio_ob) 
+  {transferway_extra <- "control failure to control success,"} 
+  if (switch_trm && !dcroddsratio_ob) 
+  {transferway_extra <- "control success to control failure,"} 
+  if (!switch_trm && dcroddsratio_ob) 
+  {transferway_extra <- "treatment success to treatment failure,"} 
+  if (!switch_trm && !dcroddsratio_ob) 
+  {transferway_extra <- "treatment failure to treatment success,"}
+  }
+
+if (invalidate_ob) {
+  change <- "To invalidate the inference,"
+} else {
+  change <- "To sustain the inference,"
+}
+
+if (!final_solution$needtworows) {
+  conclusion1 <- paste(change, sprintf("%d cases need to be transferred from", final_solution$final_switch), 
+                    transferway, c("as shown from the Implied Table to the Transfer Table."))
+} else {
+  conclusion1 <- paste(change, c("only transferring cases from"), transferway,
+                      sprintf("is not enough. We also need to transfer %d cases from", final_solution$final_extra), 
+                      transferway_extra, c("as shown from the Implied Table to the Transfer Table."))
+  
+}
+
+conclusion2 <- sprintf("For the Implied Table, we have estimate of %.3f, with standard error of %.3f and t-ratio of %.3f.", 
+                       final_solution$est_eff_start, final_solution$std_err_start, final_solution$t_start)
+conclusion3 <- sprintf("For the Transferred Table, we have estimate of %.3f, with standard error of %.3f and t-ratio of %.3f.", 
+                            final_solution$est_eff_final, final_solution$std_err_final, final_solution$t_final)
+
+result <- list(conclusion1, Implied_Table = final_solution$table_start, Transfer_Table = final_solution$table_final, 
+               conclusion2, conclusion3, Implied_Estimate = final_solution$est_eff_start, Transfer_Estimate = final_solution$est_eff_final, 
+               Implied_SE = final_solution$std_err_start, Transfer_SE = final_solution$std_err_final, 
+               Implied_tratio = final_solution$t_start, Transfer_tratio = final_solution$t_final, 
+               Taylor_predict = final_solution$taylor_pred, Percent_bias_predict = final_solution$perc_bias_pred)
+
+return(result)
+}
+
