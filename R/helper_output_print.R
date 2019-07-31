3f1dad313896988f0a44f54e03963c161cf6e74a
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-07-31 09:48:09 -0400

    add citations to output

--- R/helper_output_print.R
+++ R/helper_output_print.R
@@ -14,6 +14,10 @@ output_print <- function(eff_diff, beta_threshhold, bias = NULL, sustain = NULL,
     warning("The coefficient is exactly equal to the threshold.\n")
   }
 
+  cat("See Frank et al. (2000) for a description of the method")
+  cat("\n")
+  cat(crayon::underline("Citation:"), "Frank, K.A., Maroulis, S., Duong, M., and Kelcey, B. 2013. What would it take to change an inference?	Using Rubin’s causal model to interpret the robustness of causal inferences.", crayon::italic("Education, Evaluation and Policy Analysis, 35"), "437-460.")
+  cat("\n")
   cat("\n")
   cat(crayon::bold("Impact Threshold for a Confounding Variable:\n"))
   if (abs(obs_r) > abs(critical_r)) {
@@ -29,8 +33,9 @@ output_print <- function(eff_diff, beta_threshhold, bias = NULL, sustain = NULL,
       sep = ""
     )
     cat("Correspondingly the impact of an omitted variable (as defined in Frank 2000) must be ", r_con, " X ", r_con, " = ", round(r_con^2, 3), " to sustain an inference for a null hypothesis of 0 effect.\n", sep = "")
-  }
-  else if (obs_r == critical_r) {
+  } else if (obs_r == critical_r) {
     warning("The correlation is exactly equal to the threshold.\n")
   }
+  cat(crayon::underline("Citation:"), "Frank, K. 2000. Impact of a confounding variable on the inference of a regression coefficient.", crayon::italic("Sociological Methods and Research, 29"), "(2), 147-194")
+  cat("\n")
 }
