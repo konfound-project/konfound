e9bbfef4aca092357fb6421b3701a055f76dd02e
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-11-16 14:59:13 -0500

    update test sensitivity

--- R/test_sensitivity_ln.R
+++ R/test_sensitivity_ln.R
@@ -207,6 +207,7 @@ test_sensitivity_ln <- function(est_eff,
   # output dispatch
   if (to_return == "print") {
     return(result)
+    message("Note that this output is from an approach for non-linear models that is developmental and unpublished")
   } else {
     message("Note that only printed output is presently available for non-linear models")
   }
