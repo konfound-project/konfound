1097d2c6f6767c4e4e9ddabe14666c63b431a70e
Qinyun Lin <qinyunlin@uchicago.edu>
2020-08-25 23:03:28 -0500

    Update tkonfound_fig
    Fig2 now shows RIR; add fisher test; add base rate option - whether control group or the whole group

--- R/tkonfound_fig.R
+++ R/tkonfound_fig.R
@@ -1,12 +1,14 @@
-#' Draw figures for change in effect size as a function of switching outcomes 
-#' @description This function returns two plots for change in effect size as a function of switching outcomes (RIS),
-#' one for all possibilities, another zoomed in the area around the two thresholds  
+#' Draw figures for change in effect size as a function of switching or replacing outcomes 
+#' @description This function returns two plots for change in effect size as a function of switching or replacing outcomes,
+#' one for all possibilities (switching), another zoomed in the area for positive RIR  
 #' @param a cell is the number of cases in the control group showing unsuccessful results
 #' @param b cell is the number of cases in the control group showing successful results
 #' @param c cell is the number of cases in the treatment group showing unsuccessful results
 #' @param d cell is the number of cases in the treatment group showing successful results
 #' @param thr_p the p-value threshold used to evaluate statistical significance, with the default of 0.05
 #' @param switch_trm whether switching the two cells in the treatment row or the two cells in the control row, with the default of the treatment row
+#' @param test whether using Fisher's Exact Test (default) or chi-square test
+#' @param replace whether using entire sample or the control group to calculate the base rate, with the default of the entire sample  
 #' @importFrom stats chisq.test
 #' @return prints 2 figures for how number of hypothetical cases switched changes the effect size 
 #' @examples
@@ -14,9 +16,11 @@
 #' tkonfound_fig(35, 17, 17, 38)
 #' tkonfound_fig(35, 17, 17, 38, thr_p = 0.01)
 #' tkonfound_fig(35, 17, 17, 38, thr_p = 0.01, switch_trm = FALSE)
+#' tkonfound_fig(35, 17, 17, 38, thr_p - 0.01, switch_trm = TRUE, test = "chisq")
+#' tkonfound_fig(35, 17, 17, 38, thr_p - 0.01, switch_trm = TRUE, test = "chisq", replace = "control")
 #' @export
 
-tkonfound_fig <- function(a, b, c, d, thr_p = 0.05, switch_trm = T){
+tkonfound_fig <- function(a, b, c, d, thr_p = 0.05, switch_trm = T, test = "fisher", replace = "entire"){
 
 n_obs <- a + b + c + d
 ###***generate the log odds for each step of switch
@@ -103,13 +107,23 @@ if (switch_trm == F) {
   }
 }
 
-meta$logodds <- log(meta$a*meta$d/meta$c/meta$b)
 meta$cntrl_p <- meta$b/(meta$a + meta$b)
 meta$tr_p <- meta$d/(meta$c + meta$d)
 meta$pdif <- meta$tr_p - meta$cntrl_p
 
+
 ###***find out significant thresholds 
-solution <- getswitch_chisq(a, b, c, d, thr_p, switch_trm)
+if (test == "chisq"){
+  solution <- getswitch_chisq(a, b, c, d, thr_p, switch_trm)
+  meta$logodds <- log(meta$a*meta$d/meta$c/meta$b)
+}
+
+if (test == "fisher"){
+  solution <- getswitch_fisher(a, b, c, d, thr_p, switch_trm)
+  for (i in 1:(n_obs-3)){
+  meta$logodds[i] <- log(fisher_oddsratio(meta$a[i], meta$b[i], meta$c[i], meta$d[i]))
+  }
+}
 
 table_final <- solution$Transfer_Table
 dcroddsratio_ob <- solution$dcroddsratio_ob
@@ -122,25 +136,32 @@ if (meta[meta$sig==1,]$logodds > 0){
     posinsig <- meta[meta$sig==1,]$switch
     pos_thr <- (meta[meta$switch==posinsig,]$logodds+meta[meta$switch==(posinsig+1),]$logodds)/2
     pos_thr_pdif <- (meta[meta$switch==posinsig,]$pdif+meta[meta$switch==(posinsig+1),]$pdif)/2
-    zoom_lower <- -(posinsig + 5)
+    zoom_lower <- -(posinsig + 2)
     meta$sigpoint <- ifelse(meta$switch==(posinsig+1),"positive","other")
   } else {#from not sig to sig by increasing positive effect
     possig <- meta[meta$sig==1,]$switch
     pos_thr <- (meta[meta$switch==possig,]$logodds+meta[meta$switch==(possig-1),]$logodds)/2
     pos_thr_pdif <- (meta[meta$switch==possig,]$pdif+meta[meta$switch==(possig-1),]$pdif)/2
-    zoom_lower <- -(possig + 4)
+    zoom_lower <- -(possig + 2)
     meta$sigpoint <- ifelse(meta$switch==possig,"positive","other")
     }
   # find out the row that is cloest to logodds of 0 but negative
   temp1 <- meta[meta$logodds<0,]
   temp1 <- temp1[order(abs(temp1$logodds)),]
-  j <- 1 
-  while (chisq_p(temp1$a[j], temp1$b[j], temp1$c[j], temp1$d[j])>thr_p){
-    j <- j+1
+  j <- 1
+  if (test == "chisq"){
+    while (chisq_p(temp1$a[j], temp1$b[j], temp1$c[j], temp1$d[j])>thr_p){
+      j <- j+1
+    }
+  }
+  if (test == "fisher"){
+    while (fisher_p(temp1$a[j], temp1$b[j], temp1$c[j], temp1$d[j])>thr_p){
+      j <- j+1
+    }
   }
   neg_thr <- (temp1$logodds[j-1]+temp1$logodds[j])/2 
   neg_thr_pdif <- (temp1$pdif[j-1]+temp1$pdif[j])/2
-  zoom_upper <- -(temp1$switch[j]-4)
+  zoom_upper <- -(temp1$switch[j]-2)
   meta$sigpoint <- ifelse(meta$switch==temp1$switch[j],"negative",meta$sigpoint)
 }
 
@@ -155,31 +176,42 @@ if (meta[meta$sig==1,]$logodds < 0){
     neginsig <- meta[meta$sig==1,]$switch
     neg_thr <- (meta[meta$switch==neginsig,]$logodds+meta[meta$switch==(neginsig-1),]$logodds)/2
     neg_thr_pdif <- (meta[meta$switch==neginsig,]$pdif+meta[meta$switch==(neginsig-1),]$pdif)/2 
-    zoom_upper <- -(neginsig - 5)
+    zoom_upper <- -(neginsig - 2)
     meta$sigpoint <- ifelse(meta$switch==neginsig-1,"negative","other")
   }
   # find out the row that is cloest to logodds of 0 but positive
   temp1 <- meta[meta$logodds>0,]
   temp1 <- temp1[order(abs(temp1$logodds)),]
   j <- 1 
-  while (chisq_p(temp1$a[j], temp1$b[j], temp1$c[j], temp1$d[j])>thr_p){
-    j <- j+1
+  if (test == "chisq"){
+    while (chisq_p(temp1$a[j], temp1$b[j], temp1$c[j], temp1$d[j])>thr_p){
+      j <- j+1
+    }
+  }
+  if (test == "fisher"){
+    while (fisher_p(temp1$a[j], temp1$b[j], temp1$c[j], temp1$d[j])>thr_p){
+      j <- j+1
+    }
   }
   pos_thr <- (temp1$logodds[j-1]+temp1$logodds[j])/2 
   pos_thr_pdif <- (temp1$pdif[j-1]+temp1$pdif[j])/2
-  zoom_lower <- -(temp1$switch[j]+4)
+  zoom_lower <- -(temp1$switch[j]+2)
   meta$sigpoint <- ifelse(meta$switch==temp1$switch[j],"positive",meta$sigpoint)
 }
 
 meta$current <- ifelse(meta$switch==0, "current", "other")
 meta$sigpoint <- ifelse(meta$switch==0, "current",meta$sigpoint)
-meta$switch <- meta$switch*(-1)
-meta$label <- ifelse(meta$sigpoint=="positive", 
-                     paste("sig pos:RIS=",meta[meta$sigpoint=="positive",]$switch),NA)
-meta$label <- ifelse(meta$sigpoint=="negative", 
-                     paste("sig neg:RIS=",meta[meta$sigpoint=="negative",]$switch),meta$label)
-meta$label <- ifelse(meta$sigpoint=="current", 
-                     paste("current"),meta$label)
+
+if (switch_trm && dcroddsratio_ob) {
+  # transferway <- "treatment success to treatment failure,"
+  meta$switch <- meta$switch*(-1)
+}
+
+if (!switch_trm && dcroddsratio_ob) {
+ #transferway <- "control failure to control success,"
+  meta$switch <- meta$switch*(-1)
+}
+
 
 fillcol <-c("current"="white","positive"="green4","negative"="red","other"="white") 
 pointshape <- c("current"=15,"other"=21)
@@ -192,7 +224,7 @@ fig1 <- ggplot2::ggplot(meta, ggplot2::aes_string(x="switch", y="pdif"))+
   ggplot2::geom_hline(yintercept = pos_thr_pdif, linetype = "dashed", color="green4", size = 1)+
   ggplot2::geom_hline(yintercept = neg_thr_pdif, linetype = "dashed", color="red", size = 1)+
   ggplot2::scale_y_continuous(name="Difference in probability of successful outcome (treatment - control)")+
-  ggplot2::scale_x_continuous(name="Number of cases switched") +
+  ggplot2::scale_x_continuous(name="Number of Cases Switched - Fragility") +
   ggplot2::theme(#axis.title = ggplot2::element_text(size = 15),
         #axis.text= ggplot2::element_text(size = 14),
         panel.grid.major = ggplot2::element_blank(), 
@@ -202,25 +234,55 @@ fig1 <- ggplot2::ggplot(meta, ggplot2::aes_string(x="switch", y="pdif"))+
         legend.position = "none")
 
 zoom <- meta[meta$switch<=zoom_upper & meta$switch>=zoom_lower,]
+zoom <- zoom[zoom$switch>=0,]
 
-fig2 <- ggplot2::ggplot(zoom, ggplot2::aes_string(x="switch",y="pdif"))+
+if (switch_trm && dcroddsratio_ob) {
+  zoom$RIR <- round(zoom$switch/((a+c)/n_obs))*(replace=="entire") + round(zoom$switch/(a/(a+b)))*(1-(replace=="entire"))
+  zoom <- zoom[zoom$RIR<=d,]
+}
+if (switch_trm && !dcroddsratio_ob) {
+  zoom$RIR <- round(zoom$switch/((b+d)/n_obs))*(replace=="entire") + round(zoom$switch/(b/(a+b)))*(1-(replace=="entire"))
+  zoom <- zoom[zoom$RIR<=c,]
+}
+if (!switch_trm && dcroddsratio_ob) {
+  zoom$RIR <- round(zoom$switch/((b+d)/n_obs))*(replace=="entire") + round(zoom$switch/(b/(a+b)))*(1-(replace=="entire"))
+  zoom <- zoom[zoom$RIR<=a,]
+}
+if (!switch_trm && !dcroddsratio_ob) {
+  zoom$RIR <- round(zoom$switch/((a+c)/n_obs))*(replace=="entire") + round(zoom$switch/(a/(a+b)))*(1-(replace=="entire"))
+  zoom <- zoom[zoom$RIR<=b,]
+}
+
+zoom$xaxis <- paste(zoom$RIR,"(",zoom$switch,")",sep = "")
+zoom$label <- ifelse(zoom$sigpoint=="positive", 
+                     paste("sig pos:RIR=", zoom[zoom$sigpoint=="positive",]$RIR),NA)
+zoom$label <- ifelse(zoom$sigpoint=="negative", 
+                     paste("sig neg:RIR=", zoom[zoom$sigpoint=="negative",]$RIR),zoom$label)
+zoom$label <- ifelse(zoom$sigpoint=="current", 
+                     paste("current"),zoom$label)
+
+fig2 <- ggplot2::ggplot(zoom, ggplot2::aes_string(x="RIR",y="pdif"))+
   ggplot2::geom_line(ggplot2::aes_string(y="pdif"), size = 1) +
   ggplot2::geom_point(ggplot2::aes_string(y="pdif", shape = "current",fill = "sigpoint"), 
                       size = 3)+
   ggrepel::geom_label_repel(ggplot2::aes_string(label="label"))+
   ggplot2::scale_fill_manual(values=fillcol)+
   ggplot2::scale_shape_manual(values=pointshape)+
-  ggplot2::geom_hline(yintercept = pos_thr_pdif, linetype = "dashed", color="green4", size = 1)+
-  ggplot2::geom_hline(yintercept = neg_thr_pdif, linetype = "dashed", color="red", size = 1)+
   ggplot2::scale_y_continuous(name="Difference in probability of successful outcome (treatment - control)")+
-  ggplot2::scale_x_continuous(name="Number of cases switched") +
-  ggplot2::theme(#axis.title = ggplot2::element_text(size = 15),
-                 #axis.text= ggplot2::element_text(size = 14),
-                 panel.grid.major = ggplot2::element_blank(), 
+  ggplot2::scale_x_continuous(name="RIR (Fragility)", breaks= c(zoom$RIR),labels=zoom$xaxis) +
+  ggplot2::theme(panel.grid.major = ggplot2::element_blank(), 
                  panel.grid.minor = ggplot2::element_blank(),
                  panel.background = ggplot2::element_blank(), 
                  axis.line = ggplot2::element_line(colour = "black"),
                  legend.position = "none")
+
+if (pos_thr_pdif <= max(zoom$pdif) && pos_thr_pdif >= min(zoom$pdif)) {
+  fig2 <- fig2 + ggplot2::geom_hline(yintercept = pos_thr_pdif, linetype = "dashed", color="green4", size = 1)
+}
+
+if (neg_thr_pdif <= max(zoom$pdif) && neg_thr_pdif >= min(zoom$pdif)) {
+  fig2 <- fig2 + ggplot2::geom_hline(yintercept = neg_thr_pdif, linetype = "dashed", color="red", size = 1)
+}
 
 ###plot figure 3 RIS% as sample size gets larger, using t statistic as the criterion
 # if (plt3 == TRUE) {
@@ -247,14 +309,14 @@ fig2 <- ggplot2::ggplot(zoom, ggplot2::aes_string(x="switch",y="pdif"))+
 #          axis.text = element_text(size = 12))
 #} 
 
-if (switch_trm == T){
+if (switch_trm == T) {
   note <- "A bend in line indicates switches from the control row because the treatment row was exhausted."
-  }
-else {
+  } else {
   note <- "A bend in line indicates switches from the treatment row because the control row was exhausted."
-}
+  }
 
 result <- list(fig1, note, fig2)
 
 return(result)
 }
+
