db1f1f917141229b6c92e268059b7b0953b927e0
Joshua Rosenberg <jmrosen48@gmail.com>
2020-11-29 15:11:26 -0500

    Auto stash before revert of "first working version of `pkonfound()` with `tkonfound` integrated"

---
+++ R/concord1-data.R
@@ -0,0 +1,12 @@
+#' Water use survey data by Hamilton (1983) from Concord, New Hampshire
+#'
+#' @docType data
+#'
+#' @usage data(concord1)
+#'
+#' @keywords datasets
+#'
+#' @references Hamilton, L.C., 1992. Regression with graphics: A second course in applied statistics. Brooks/Cole Pub. Co.
+#'
+#' @examples
+#' data(concord1)
