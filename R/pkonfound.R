d7f513b86e47eca4cbeb8b472dd046925e4a5ce8
Joshua Rosenberg <jmrosen48@gmail.com>
2020-11-29 15:08:17 -0500

    reset to last working version, keep data files added

--- R/pkonfound.R
+++ R/pkonfound.R
@@ -7,15 +7,18 @@
 #' @param alpha probability of rejecting the null hypothesis (defaults to 0.05)
 #' @param tails integer whether hypothesis testing is one-tailed (1) or two-tailed (2; defaults to 2)
 #' @param nu what hypothesis to be tested; defaults to testing whether est_eff is significantly different from 0
-#' @param dichotomous whether the model is a non-linear model; defaults to FALSE
-#' @param n_trm the number of cases associated with the treatment condition; applicable only when dichotomous = TRUE
-#' @param switch_trm whether to switch the treatment and control cases; defaults to FALSE; applicable only when dichotomous - TRUE
+
+#' @param non_linear whether the model is a non-linear model; defaults to FALSE
+#' @param n_trm the number of cases associated with the treatment condition; applicable only when non_linear = TRUE
+#' @param switch_trm whether to switch the treatment and control cases; defaults to FALSE; applicable only when non_linear - TRUE
+
 #' @param a cell is the number of cases in the control group showing unsuccessful results
 #' @param b cell is the number of cases in the control group showing successful results
 #' @param c cell is the number of cases in the treatment group showing unsuccessful results
 #' @param d cell is the number of cases in the treatment group showing successful results
-#' @param test whether using Fisher's Exact Test or A chi-square test; defaults to Fisher's Exact Test
-#' @param replace whether using entire sample or the control group to calculate the base rate; default is the entire sample
+#' @param test whether using Fisher's Exact Test (default) or chi-square test
+#' @param replace whether using entire sample or the control group to calculate the base rate, with the default of the entire sample  
+
 #' @param to_return whether to return a data.frame (by specifying this argument to equal "raw_output" for use in other analyses) or a plot ("plot"); default is to print ("print") the output to the console; can specify a vector of output to return
 #' @return prints the bias and the number of cases that would have to be replaced with cases for which there is no effect to invalidate the inference
 #' @examples
@@ -23,7 +26,7 @@
 #' pkonfound(2, .4, 100, 3)
 #' pkonfound(-2.2, .65, 200, 3)
 #' pkonfound(.5, 3, 200, 3)
-#' pkonfound(-0.2, 0.103, 20888, 3, n_trm = 17888, dichotomous = TRUE)
+#' pkonfound(-0.2, 0.103, 20888, 3, n_trm = 17888, non_linear = TRUE)
 #'
 #' pkonfound(2, .4, 100, 3, to_return = "thresh_plot")
 #' pkonfound(2, .4, 100, 3, to_return = "corr_plot")
@@ -41,33 +44,29 @@
 #' tkonfound(35, 17, 17, 38, thr_p = 0.01)
 #' 
 #' # using pkonfound for a 2x2 table
-#' pkonfound(35, 17, 17, 38)
-#' pkonfound(35, 17, 17, 38, thr_p = 0.01)
-#' pkonfound(35, 17, 17, 38, thr_p = 0.01, switch_trm = FALSE)
-#' pkonfound(35, 17, 17, 38, test = "chisq")
+#' tkonfound(35, 17, 17, 38)
+#' tkonfound(35, 17, 17, 38, thr_p = 0.01)
+#' tkonfound(35, 17, 17, 38, thr_p = 0.01, switch_trm = FALSE)
+#' tkonfound(35, 17, 17, 38, test = "chisq")
 #'
+
 #' @export
 
-pkonfound <- function(est_eff = NULL,
-                      std_err = NULL,
-                      n_obs = NULL,
+pkonfound <- function(est_eff,
+                      std_err,
+                      n_obs,
                       n_covariates = 1,
                       alpha = .05,
                       tails = 2,
                       nu = 0,
                       n_trm = NULL,
                       switch_trm = TRUE,
-                      dichotomous = FALSE,
-                      a = NULL,
-                      b = NULL,
-                      c = NULL,
-                      d = NULL,
-                      test = "fisher",
-                      replace = "control",
+                      replace = "entire",
+                      non_linear = FALSE,
                       to_return = "print") {
   if ("table" %in% to_return) stop("a table can only be output when using konfound")
   # if (nu != 0) warning("Output for the impact of the confounding variable (the ITCV) is not valid for a non-0 null hypothesis about an effect")
-  if (dichotomous == TRUE) {
+  if (non_linear == TRUE) {
     out <- test_sensitivity_ln(
       est_eff = est_eff,
       std_err = std_err,
@@ -81,22 +80,7 @@ pkonfound <- function(est_eff = NULL,
       switch_trm = switch_trm,
       replace = replace
     )
-  } else if(!is.null(a)) {
-    # error handling
-    if (is.null(a) | is.null(b) | is.null(c) | is.null(d)) {
-      stop("Please enter values for a, b, c, and d to use the 2 x 2 table functionality")
-    }
-    
-    out <- tkonfound(a = a, 
-                     b = b, 
-                     c =c, 
-                     d = d, 
-                     alpha = alpha, 
-                     switch_trm = switch_trm,
-                     test = test, 
-                     replace = replace)
-    
-  }  else {
+  } else {
     out <- test_sensitivity(
       est_eff = est_eff,
       std_err = std_err,
@@ -108,14 +92,14 @@ pkonfound <- function(est_eff = NULL,
       to_return = to_return
     )
   }
-  
+
   if (!is.null(out)) { # dealing with a strange print issue
     return(out)
   }
-  
+
   if (to_return == "print") {
     message("For other forms of output, change `to_return` to table, raw_output, thres_plot, or corr_plot.")
   }
-  
+
   message("For models fit in R, consider use of konfound().")
 }
