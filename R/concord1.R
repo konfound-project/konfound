c6eda2f97f2c7bcd76ed2fc62bd475a63aca346c
Joshua Rosenberg <jmrosen48@gmail.com>
2020-12-01 15:15:36 -0500

    update docs for data

---
+++ R/concord1.R
@@ -0,0 +1,15 @@
+#'  Concord1 data
+#'
+#' This data .. XXX
+#'
+#' \tabular{lll}{
+#'   \strong{sex} \tab \code{factor} \tab Self-identified sex of participants, Male, Female, or Transgendered.\cr
+#'   \strong{age_years} \tab \code{numeric} \tab Participants' age in years.\cr
+#' }
+#' @docType data
+#' @name concord1
+#' @usage data(concord1)
+#' @references Curry, O. S., Jones Chesters, M., & Van Lissa, C. J. (2019). Mapping morality with a compass: Testing the theory of ‘morality-as-cooperation’ with a new questionnaire. Journal of Research in Personality, 78, 106–124.
+#' \href{https://doi.org/10.1016/j.jrp.2018.10.008}{DOI:10.1016/j.jrp.2018.10.008}
+#' @format A data.frame with 496 rows and 10 variables.
+NULL
