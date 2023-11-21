50da8c90f574cedb3e1de9753be515bfb23ed5cd
wwang93 <142034236+wwang93@users.noreply.github.com>
2023-11-21 09:22:20 -0500

    Update pkonfound.R
    Manual modification

--- R/pkonfound.R
+++ R/pkonfound.R
@@ -1,34 +1,59 @@
 #' Perform sensitivity analysis for published studies
-#' @description For published studies, this command calculates (1) how much bias there must be in an estimate to invalidate/sustain an inference; (2) the impact of an omitted variable necessary to invalidate/sustain an inference for a regression coefficient.
-#' @param est_eff the estimated effect (such as an unstandardized beta coefficient or a group mean difference)
-#' @param std_err the standard error of the estimate of the unstandardized regression coefficient
+#' @description For published studies, this command calculates 
+#' (1) how much bias there must be in an estimate to invalidate/sustain 
+#' an inference; (2) the impact of an omitted variable necessary to 
+#' invalidate/sustain an inference for a regression coefficient.
+#' @param est_eff the estimated effect (such as an unstandardized beta 
+#' coefficient or a group mean difference)
+#' @param std_err the standard error of the estimate of the unstandardized 
+#' regression coefficient
 #' @param n_obs the number of observations in the sample
 #' @param n_covariates the number of covariates in the regression model
 #' @param alpha probability of rejecting the null hypothesis (defaults to 0.05)
-#' @param tails integer whether hypothesis testing is one-tailed (1) or two-tailed (2; defaults to 2)
-#' @param index whether output is RIR or IT (impact threshold); defaults to "RIR"
-#' @param nu what hypothesis to be tested; defaults to testing whether est_eff is significantly different from 0
-#' @param model_type the type of model being estimated; defaults to "ols" for a linear regression model; the other option is "logistic"
-#' @param n_treat the number of cases associated with the treatment condition; applicable only when model_type = "logistic"
-#' @param switch_trm whether to switch the treatment and control cases; defaults to FALSE; applicable only when model_type = "logistic"
-#' @param a cell is the number of cases in the control group showing unsuccessful results
-#' @param b cell is the number of cases in the control group showing successful results
-#' @param c cell is the number of cases in the treatment group showing unsuccessful results
-#' @param d cell is the number of cases in the treatment group showing successful results
-#' @param two_by_two_table table that is a matrix or can be coerced to one (data.frame, tibble, tribble) from which the a, b, c, and d arguments can be extracted
-#' @param test whether using Fisher's Exact Test or A chi-square test; defaults to Fisher's Exact Test
-#' @param replace whether using entire sample or the control group to calculate the base rate; default is the control group
+#' @param tails integer whether hypothesis testing is one-tailed (1) or 
+#' two-tailed (2; defaults to 2)
+#' @param index whether output is RIR or IT (impact threshold); 
+#' defaults to "RIR"
+#' @param nu what hypothesis to be tested; defaults to testing whether 
+#' est_eff is significantly different from 0
+#' @param model_type the type of model being estimated; defaults to "ols" for 
+#' a linear regression model; the other option is "logistic"
+#' @param n_treat the number of cases associated with the treatment condition; 
+#' applicable only when model_type = "logistic"
+#' @param switch_trm whether to switch the treatment and control cases; defaults
+#'  to FALSE; applicable only when model_type = "logistic"
+#' @param a cell is the number of cases in the control group showing 
+#' unsuccessful results
+#' @param b cell is the number of cases in the control group showing successful 
+#' results
+#' @param c cell is the number of cases in the treatment group showing 
+#' unsuccessful results
+#' @param d cell is the number of cases in the treatment group showing 
+#' successful results
+#' @param two_by_two_table table that is a matrix or can be coerced to one 
+#' (data.frame, tibble, tribble) from which the a, b, c, and d arguments can 
+#' be extracted
+#' @param test whether using Fisher's Exact Test or A chi-square test; defaults 
+#' to Fisher's Exact Test
+#' @param replace whether using entire sample or the control group to calculate
+#'  the base rate; default is the control group
 #' @param sdx the standard deviation of X
 #' @param sdy the standard deviation of Y
 #' @param R2 the unadjusted,original R2 in the observed function
 #' @param eff_thr unstandardized coefficient threshold to change an inference
-#' @param FR2max the largest R2, or R2max, in the final model with unobserved confounder 
-#' @param FR2max_multiplier the multiplier of R2 to get R2max, default is set to 1.3
-#' @param to_return whether to return a data.frame (by specifying this argument to equal "raw_output" for use in other analyses) or a plot ("plot"); default is to print ("print") the output to the console; can specify a vector of output to return
+#' @param FR2max the largest R2, or R2max, in the final model with unobserved 
+#' confounder 
+#' @param FR2max_multiplier the multiplier of R2 to get R2max, 
+#' default is set to 1.3
+#' @param to_return whether to return a data.frame 
+#' (by specifying this argument to equal "raw_output" for use in other analyses)
+#'  or a plot ("plot"); default is to print ("print") the output to the console;
+#'   can specify a vector of output to return
 #' @importFrom stats fisher.test
 #' @import lavaan
 #' @import ggplot2
-#' @return prints the bias and the number of cases that would have to be replaced with cases for which there is no effect to invalidate the inference
+#' @return prints the bias and the number of cases that would have to be 
+#' replaced with cases for which there is no effect to invalidate the inference
 #' @examples
 #' # using pkonfound for linear models
 #' pkonfound(2, .4, 100, 3)
@@ -62,9 +87,11 @@
 # pkonfound(two_by_two_table = my_table)
 #
 #' # use pkonfound to calculate delta* and delta_exact 
-#' pkonfound(est_eff = .4, std_err = .1, n_obs = 290, sdx = 2, sdy = 6, R2 = .7, eff_thr = 0, FR2max = .8, index = "COP", to_return = "raw_output")
+#' pkonfound(est_eff = .4, std_err = .1, n_obs = 290, sdx = 2, sdy = 6, R2 = .7,
+#'  eff_thr = 0, FR2max = .8, index = "COP", to_return = "raw_output")
 #' # use pkonfound to calculate rxcv and rycv when preserving standard error
-#' pkonfound(est_eff = .5, std_err = .056, n_obs = 6174, eff_thr = .1, sdx = 0.22, sdy = 1, R2 = .3, index = "PSE", to_return = "raw_output")
+#' pkonfound(est_eff = .5, std_err = .056, n_obs = 6174, eff_thr = .1, 
+#' sdx = 0.22, sdy = 1, R2 = .3, index = "PSE", to_return = "raw_output")
 #' @export
 
  pkonfound <- function(est_eff,
@@ -92,7 +119,8 @@
                       FR2max,
                       FR2max_multiplier = 1.3,
                       to_return = "print") {
-  if ("table" %in% to_return) stop("a table can only be output when using konfound")
+  if ("table" %in% to_return) stop("a table can only be 
+                                   output when using konfound")
   
    if (index == "COP") {
      
@@ -139,7 +167,10 @@
     )
   } else  if (model_type == "logistic" & is.null(n_treat)) {
     
-    warning("For a logistic regression, the log-odds coefficients are used for the pkonfound() calculations. To carry out an analysis using average marginal effects, you can use the konfound() function with the results of a model estimated in R.")
+    warning("For a logistic regression, the log-odds coefficients 
+            are used for the pkonfound() calculations. To carry out an 
+            analysis using average marginal effects, you can use the 
+            konfound() function with the results of a model estimated in R.")
     
     out <- test_sensitivity_ln(
       est_eff = est_eff,
@@ -157,7 +188,8 @@
   } else if(!is.null(a)) {
     # error handling
     if (is.null(a) | is.null(b) | is.null(c) | is.null(d)) {
-      stop("Please enter values for a, b, c, and d to use the 2 x 2 table functionality")
+      stop("Please enter values for a, b, c, 
+           and d to use the 2 x 2 table functionality")
     }
     
     out <- tkonfound(a = a, 
@@ -208,7 +240,8 @@ if (!is.null(out)) { # dealing with a strange print issue
 }
 
 if (to_return == "print") {
-  message("For other forms of output, run ?pkonfound and inspect the to_return argument")
+  message("For other forms of output, run ?
+          pkonfound and inspect the to_return argument")
 }
 
 message("For models fit in R, consider use of konfound().")
