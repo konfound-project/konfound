616a81678aca58292b561b3a3a6e3f9b6db7986f
Jihoon_Choi <50298939+JihoonChoi26@users.noreply.github.com>
2024-02-02 15:19:14 -0500

    Update helper_output_print.R

--- R/helper_output_print.R
+++ R/helper_output_print.R
@@ -178,7 +178,7 @@ if (requireNamespace("htmltools", quietly = TRUE)) {
     
     link_html <- '<html><body><a href="https://journals.sagepub.com/doi/10.1177/0049124100029002001">Click here for the article</a></body></html>'
     if (requireNamespace("htmltools", quietly = TRUE)) {
-      invisible(htmltools::html_print(htmltools::HTML(link_html)))
+      capture.output(htmltools::html_print(htmltools::HTML(link_html)), file = NULL)
     } else {
       message("htmltools package is required")
     }
