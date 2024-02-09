2810fd0ef397c3098e03f2527d9e23a98c6d12bd
jrosen48 <jmrosen48@gmail.com>
2024-02-09 08:03:29 -0500

    undo lme4 edits
    trying another approach

--- tests/testthat/test-konfound.R
+++ tests/testthat/test-konfound.R
@@ -11,8 +11,8 @@ output1 <- konfound(m1, wt, to_return = "raw_output")
 # m2 <- glm(married ~ age, data = d, family = binomial(link = "logit"))
 # output2 <- konfound(m2, age, to_return = "raw_output")
 
-m3 <- fm1 <- lme4::lmer(Reaction ~ Days + (1 | Subject), sleepstudy)
-output3 <- konfound(m3, Days, to_return = "raw_output")
+# m3 <- fm1 <- lme4::lmer(Reaction ~ Days + (1 | Subject), sleepstudy)
+# output3 <- konfound(m3, Days, to_return = "raw_output")
 
 m4 <- glm(outcome ~ condition, data = binary_dummy_data, family = binomial(link = "logit"))
 output4 <- konfound(m4, condition, two_by_two = TRUE, n_treat = 55, to_return = "raw_output")
@@ -28,9 +28,9 @@ output4 <- konfound(m4, condition, two_by_two = TRUE, n_treat = 55, to_return = 
 
 ## these both need updating
 
-test_that("konfound works for lme4 model", {
-    expect_equal(output3$percent_bias_to_change_inference, 84.826, tolerance = .001)
-})
+# test_that("konfound works for lme4 model", {
+#     expect_equal(output3$percent_bias_to_change_inference, 84.826, tolerance = .001)
+# })
 
 test_that("konfound works for glm, 2x2 model", {
     expect_equal(output4$RIR, 15)
--- tests/testthat/test-mkonfound.r
+++ tests/testthat/test-mkonfound.r
@@ -6,12 +6,12 @@ library(mice)
 popmis <- popmis[1:100, ]
 
 testmod1 <- lm(teachpop ~ texp + sex, data = popmis)
-testmod2 <- lmer(teachpop ~ texp + sex + (1 | school), data = popmis)
+# testmod2 <- lmer(teachpop ~ texp + sex + (1 | school), data = popmis)
 
 output1 <- konfound(testmod1, texp, test_all = TRUE, to_return = "raw_output")
-output2 <- konfound(testmod2, texp, test_all = TRUE, to_return = "raw_output")
+# output2 <- konfound(testmod2, texp, test_all = TRUE, to_return = "raw_output")
 
-test_that("mkonfound works for lm and lmer outout", {
+test_that("mkonfound works for lm and outout", {
   expect_is(output1, "data.frame")
-  expect_is(output2, "data.frame")
+  # expect_is(output2, "data.frame")
 })
