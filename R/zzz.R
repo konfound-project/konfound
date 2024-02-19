6135139b8b83b1014ecb58d7429656b8e83f4cc2
wwang93 <142034236+wwang93@users.noreply.github.com>
2024-02-18 23:47:03 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -12,5 +12,7 @@ if (getRversion() >= "2.15.1") utils::globalVariables(c("inference", "key",
                                                         "percent_bias", "val"))
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Sensitivity analysis as described in Frank, Maroulis, Duong, and Kelcey (2013) and in Frank (2000).\nFor more information visit http://konfound-it.com.")
+  packageStartupMessage("Sensitivity analysis as described in Frank, 
+Maroulis, Duong, and Kelcey (2013) and in 
+Frank (2000).\nFor more information visit http://konfound-it.com.")
 }
