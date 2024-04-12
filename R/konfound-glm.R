ce62c7b7b02852eda1482582e5eb9d0b005f1fe6
Joshua Rosenberg <jmrosen48@gmail.com>
2024-04-12 11:28:17 -0400

    remove margins due to an urgent issue

--- R/konfound-glm.R
+++ R/konfound-glm.R
@@ -28,9 +28,10 @@ konfound_glm <- function(model_object,
     
     coef_df <- tidy_output[tidy_output$term == tested_variable_string, ]
     est_eff <- coef_df$estimate
-    est_eff <- suppressWarnings(
-      summary(margins::margins(model_object))$AME[names(summary(
-        margins::margins(model_object))$AME) == tested_variable_string])
+    warning("Due to an issue with the margins and predictions package, these are the raw coefficients, not the average marginal effects; we will address this in future patches")
+    # est_eff <- suppressWarnings(
+    #   summary(margins::margins(model_object))$AME[names(summary(
+    #     margins::margins(model_object))$AME) == tested_variable_string])
     std_err <- coef_df$std.error
     n_obs <- glance_output$nobs
     n_covariates <- glance_output$df.null - glance_output$df.residual
