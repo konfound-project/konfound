ebe8c080d896a657ad5e37001f444d2e4b346d81
Qinyun Lin <qinyun.lin@gu.se>
2024-07-07 20:17:05 +0000

    update test given updated output

--- tests/testthat/test-cop_pse_auxiliary.R
+++ tests/testthat/test-cop_pse_auxiliary.R
@@ -3,7 +3,7 @@ context("Checking auxiliary")
 test_that("cal_ryz works", {
     expect_equal(cal_ryz(0.1, 0.6), tolerance = .001, 0.7719842) 
     expect_equal(cal_ryz(0.2, 0.6), tolerance = .001, 0.7637626) 
-    expect_error(expect_message(cal_ryz(0.6, 0.2)), "Error! R2yz < 0!")
+    expect_error(expect_message(cal_ryz(0.6, 0.2)), "The calculated variance in Y explained by Z is less than 0. This can occur if Z\n suppresses the relationship between X and Y. That is, if partialling on Z increases\n the relationship between X and Y. The unconditional ITCV is not conceptualized for\n this scenario.")
 })
 
 test_that("cal_rxz works", {
