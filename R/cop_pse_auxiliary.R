1ef54f94d897be7778e341c9b112cb572c203677
Qinyun Lin <qinyun.lin@gu.se>
2024-07-24 18:21:20 +0200

    update warning message for

--- R/cop_pse_auxiliary.R
+++ R/cop_pse_auxiliary.R
@@ -9,7 +9,7 @@ cal_ryz <- function(ryxGz, R2){
     if (R2yz >= 0) {
         ryz <- sqrt(R2yz)
     } else {
-        stop("The calculated variance in Y explained by Z is less than 0. This can occur if Z\n suppresses the relationship between X and Y. That is, if partialling on Z increases\n the relationship between X and Y. The unconditional ITCV is not conceptualized for\n this scenario.")
+        stop("The calculated variance in Y explained by Z is less than 0. This can occur if Z\n suppresses the relationship between X and Y. That is, if partialling on Z increases\n the relationship between X and Y. Note: the unconditional ITCV is not conceptualized\n for this scenario.")
     }
     return(ryz)
 }
