a0fa15a6369c2a23cb5a2e870690ea4606031e19
Qinyun Lin <qinyun.lin@gu.se>
2024-11-25 23:23:45 +0100

    fix errors after simulation verification
    1. double check with Ken cal_ryz does not need sdx and sdy
    2. sustain scenarios are not considered for fixed value and null distribution

--- R/test_cRIR.R
+++ R/test_cRIR.R
@@ -1,31 +1,52 @@
 # Create the new test_cRIR function
-test_cRIR <- function(est_eff, std_err, n_obs, n_covariates = 1, sdx = NA, sdy = NA, R2 = NA, alpha = 0.05, tails = 2, to_return = "print") {
-  # Calculate cond_RIRpi_null by default
-  cond_RIRpi_null <- est_eff / (std_err * sqrt(n_obs)) * 100
-  cond_RIR_null <- cond_RIRpi_null * n_obs / 100
-  
-  # If sdx, sdy, and R2 are provided, calculate other conditional RIR measures
-  cond_RIRpi_fixedY <- NA
-  cond_RIRpi_rxyz <- NA
-  cond_RIR_fixedY <- NA
-  cond_RIR_rxyz <- NA
-  
-  
-  if (!is.na(sdx) && !is.na(sdy) && !is.na(R2)) {
-    cond_RIRpi_fixedY <- (est_eff / sdx) * (1 - R2) * 100
-    cond_RIRpi_rxyz <- (est_eff / (sdy * sqrt(1 - R2))) * 100
-    cond_RIR_fixedY <- cond_RIRpi_fixedY * n_obs / 100
-    cond_RIR_rxyz <- cond_RIRpi_rxyz * n_obs / 100
-  }
-  
+test_cRIR <- function(est_eff, std_err, n_obs, n_covariates = 1, 
+                      # sdx = NA, sdy = NA, 
+                      R2 = NA, alpha = 0.05, tails = 2, to_return = "print") {
+    # calculating critical r
+    if (est_eff < 0) {
+        critical_t <- stats::qt(1 - (alpha / tails), n_obs - n_covariates - 2) * -1
+    } else {
+        critical_t <- stats::qt(1 - (alpha / tails), n_obs - n_covariates - 2)
+    }
+    critical_r <- critical_t / sqrt((critical_t^2) + (n_obs - n_covariates - 2))
+    
+    ## calculate ryxGz or observed r
+    ryxGz <- (est_eff / std_err) / sqrt(((n_obs - n_covariates - 2) + ((est_eff / std_err)^2)))
+    
+    # calculate cond_RIRpi_rxyz by default 
+    if (abs(ryxGz) > abs(critical_r)) {
+        ## this is equal to regular correlation based RIR 
+        cond_RIRpi_rxyz <- (1 - critical_r/ryxGz) * 100
+        cond_RIR_rxyz <- cond_RIRpi_rxyz * n_obs / 100
+    } else if (abs(ryxGz) < abs(critical_r)) {
+        cond_RIRpi_rxyz <- (1 - ryxGz/critical_r) * 100
+        cond_RIR_rxyz <- cond_RIRpi_rxyz * n_obs / 100
+    }
+    
+    # If sdx, sdy, and R2 are provided, calculate other conditional RIR measures
+    cond_RIRpi_fixedY <- NA
+    cond_RIRpi_null <- NA
+    cond_RIR_fixedY <- NA
+    cond_RIR_null <- NA
+    
+    if (!is.na(R2)) {
+        ryz <- rzy <- cal_ryz(ryxGz, R2)
+        # final solutions 
+        cond_RIRpi_fixedY <- (R2 - ryz^2 + ryz^2 * critical_r^2 - critical_r^2) / 
+            (R2 - ryz^2 + ryz^2 * critical_r^2) * 100
+        cond_RIR_fixedY <- cond_RIRpi_fixedY * n_obs / 100
+        cond_RIRpi_null <- (1 - sqrt(critical_r^2 / 
+                                         (R2 - ryz^2 + ryz^2 * critical_r^2))) * 100
+        cond_RIR_null <- cond_RIRpi_null * n_obs / 100
+    }
   
   # Return results based on user's preference
   if (to_return == "print") {
     cat(crayon::bold("Robustness of Inference to Replacement (RIR):\n"))
     cat("This application of RIR quantifies how much data we expect to have to replace when the properties of the replacement data are specified as conditional on other terms already in the model.")  
-    cat(sprintf("If the replacement data points follow a null distribution, then RIR = %.3f.\n", cond_RIR_null))
+    cat(sprintf("If the replacement data points satisfy rxy|Z = 0, then RIR = %.3f.\n", cond_RIR_rxyz))
     if (!is.na(cond_RIR_fixedY)) cat(sprintf("If the replacement data points have a fixed value, then RIR = %.3f.\n", cond_RIR_fixedY))
-    if (!is.na(cond_RIR_rxyz)) cat(sprintf("If the replacement data points satisfy rxy|Z = 0, then RIR = %.3f.\n", cond_RIR_rxyz))
+    if (!is.na(cond_RIR_null)) cat(sprintf("If the replacement data points follow a null distribution, then RIR = %.3f.\n", cond_RIR_null))
     } else if (to_return == "raw_output") {
           return(output_list(obs_r = NA, act_r = NA,
                              # act_r only makes sense when nu!=0
